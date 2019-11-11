f0677602a1a297d50dfecf2e41313d7e266a1f23
Jack Hutchings <jahutch2@gmail.com>
2019-11-11 10:14:32 -0600

    Switched to log-transformed calibration due to vastly improved residuals on low end of calibration. Since the FAME calibration mix is only C24-C32 evens, the means of their slope/intercept are applied to all other FAMEs.

--- GC-FID FAMEs Processing Script.R
+++ GC-FID FAMEs Processing Script.R
@@ -33,29 +33,31 @@ parameters <- spread(na.omit(raw_data$parameters),Parameter,Value)
 
 #########  STEP 1  ##############   Calibration Curves   ###################################
 
-calib <- raw_data$calib[,c(1:3)] %>% 
+calib <- raw_data$calib %>% 
   full_join(raw_data$mix.std,by="comp") %>%  na.omit() %>% mutate(dummy=T) %>% 
   full_join(parameters %>% mutate(dummy=T),by="dummy") %>% select(-dummy) %>% 
-  mutate(ug.injected = (conc)* #ug/mL
-           (dil.factor^dil)* #unitless, thus still ug/mL
-           (1/1000)* #unit conversion, now ug/uL
-           (inj.vol) #volume injected, now ug
+  mutate(ng.injected = (conc)* #ng/uL
+           (dil.factor^dil)* #unitless, thus still ng/uL
+           (inj.vol) #volume injected, now ng
   ) %>% 
-  select(comp,dil,area,ug.injected)
+  select(batch,comp,dil,area,ng.injected) %>% 
+  mutate(log_inj = log(ng.injected),
+         log_area = log(area))
 
-calib.sum <- group_by(calib,comp) %>% do(fit=lm(ug.injected ~ area,.)) %>%
-  tidy(fit) %>% select(comp,term,estimate) %>% spread(term,estimate) %>% setNames(.,c("comp","intercept","slope")) %>%
-  gather(coef,value,intercept,slope) %>% group_by(coef) %>% mutate(mean = mean(value)) %>% spread(comp,value) %>%
-  gather(comp,value,-coef) %>% spread(coef,value)
+calib.sum <- group_by(calib,batch,comp) %>% do(fit=lm(log_inj ~ log_area,.)) %>%
+  tidy(fit) %>% select(batch,comp,term,estimate) %>% spread(term,estimate) %>% setNames(.,c("batch","comp","intercept","slope")) %>%
+  gather(coef,value,intercept,slope) %>% group_by(batch,coef) %>% mutate(mean = mean(value)) %>% spread(comp,value) %>%
+  gather(comp,value,-c(coef,batch)) %>% spread(coef,value)
 
-calib.check <- full_join(calib.sum %>% filter(comp!="mean"),calib,by="comp") %>%
-  mutate(residual = (slope*area+intercept) - ug.injected,
-         relative.residual = residual/ug.injected*100) %>% select(dil,comp,intercept,slope,residual,relative.residual) 
+calib.check <- full_join(calib.sum %>% filter(comp!="mean"),calib,by=c("batch","comp")) %>%
+  mutate(residual = exp((slope*log_area+intercept)) - ng.injected,
+         relative.residual = residual/ng.injected*100) %>% select(batch,dil,comp,intercept,slope,residual,relative.residual) 
+
 
 #Calibration Plot
-ggplot(calib,aes(area,ug.injected)) +
+ggplot(calib,aes(log_area,log_inj,color=batch)) +
   geom_point() +
-  stat_smooth(method = "lm", se = F,formula = y ~ x,color="blue") +
+  stat_smooth(method = "lm", se = F,formula = y ~ x) +
   facet_wrap(~comp,scales="free") +
   theme_bw() +
   theme(axis.title = element_text(size=18, vjust=0.25),
@@ -67,34 +69,32 @@ ggplot(calib,aes(area,ug.injected)) +
 #Residual Plot
 calib.check %>% 
   ggplot(aes(dil,relative.residual)) +
-  geom_point() +
+  geom_bar(stat = "identity",position = "dodge") +
   facet_wrap(~comp,scales = "free_y") +
-  labs(x = "Dilution #", y = "Relative Residual (%)")
+  labs(x = "Dilution #", y = "Relative Residual")
 
 #Clean-up from the calibration section
-rm(calib,calib.check,calib.stats)
+rm(calib,calib.check)
 
 #######  STEP 2  #################   Data Analysis  ########################################
 
-data <- raw_data$gcfid %>% full_join(calib.sum,by="comp") %>%
+data <- raw_data$gcfid %>% 
+  spread(comp,area) %>% gather(comp,area,-c(id1,id2)) %>% mutate(area = ifelse(is.na(area),0,area)) %>% 
   full_join(raw_data$samples,by = c("id1", "id2")) %>%
-  mutate(intercept = ifelse(is.na(intercept),intercept[which(comp=="mean")],intercept),
-         slope = ifelse(is.na(slope),slope[which(comp=="mean")],slope)) %>%
+  full_join(calib.sum,by=c("batch","comp")) %>%
+  mutate(slope = ifelse(is.na(slope),slope[which(comp=="mean")],slope),
+         intercept = ifelse(is.na(intercept),intercept[which(comp=="mean")],intercept)) %>% 
   filter(comp!="mean") %>% 
   mutate(dummy=T) %>% full_join(parameters %>% mutate(dummy=T),by="dummy") %>% select(-dummy) %>% 
-  mutate(ug.injected = slope * area + intercept,
+  mutate(ng.injected = exp(log(area) * slope + intercept),
+         ng.injected = ifelse(area == 0,0,ng.injected),
          irs.theoretical = irs / irs.vol * irs.spike / volume * inj.vol) %>% group_by(id1,id2) %>% 
-  mutate(ug.total = ug.injected / inj.vol * volume) %>% # Placeholder ug.total calculation for samples without IRS
-  # mutate(recovery = ug.injected[which(comp == "EVAL")] / eval.theoretical,
-  #        mg.total = ug.injected / recovery / inj.vol * residue.vol) %>%
-  # select(sample,rep,comp,recovery,ug.total) %>% 
-  mutate(uggs = ug.total / mass_mg * 1000, #micrograms per gram sediment
-         uggs = ifelse(is.na(uggs),0,uggs)) %>% 
+  mutate(ug.total = ng.injected / inj.vol * volume / 1000) %>% 
+  mutate(uggs = ug.total / mass_mg * 1000) %>%  #micrograms per gram sediment
   select(id1,id2,carbon_toc,comp,uggs) %>% 
-  spread(comp,uggs) %>% gather(comp,uggs,-c(id1,id2,carbon_toc)) %>% 
-  mutate(uggs = ifelse(is.na(uggs),0,uggs)) %>% spread(comp,uggs) %>% 
+  spread(comp,uggs) %>% 
   mutate(
-    sigmaLCFA = sum(C22:C32),
+    sigmaLCFA = sum(C22,C23,C24,C25,C26,C27,C28,C29,C30,C31,C32),
     lambdaLCFA = sigmaLCFA*100/carbon_toc,
     ACL = (22*C22+24*C24+26*C26+28*C28+30*C30+32*C32+34*C34)/(C22+C24+C26+C28+C30+C32+C34),
     CPI = (C22+C24+C26+C28+C30+C32+C34)/(C21+C23+C25+C27+C29+C31+C33)

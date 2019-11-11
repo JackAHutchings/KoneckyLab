2b25bd0c4677a03c777dfcef8e90c4ce8450fcba
Jack Hutchings <jahutch2@gmail.com>
2019-11-11 09:57:22 -0600

    Two main changes: (1) added a 'batch' identifier to process multiple runs at once using their own calibrations and (2) calibration is now done using log-transformed values. Even though the FID is linear, log-transformation provides equal weights and, thus, much nicer residuals.

--- GC-FID n-Alkanes Processing Script.R
+++ GC-FID n-Alkanes Processing Script.R
@@ -34,30 +34,31 @@ parameters <- spread(na.omit(raw_data$parameters),Parameter,Value)
 
 #########  STEP 1  ##############   Calibration Curves   ###################################
 
-calib <- raw_data$calib[,c(1:3)] %>% 
+calib <- raw_data$calib %>% 
   full_join(raw_data$mix.std,by="comp") %>%  na.omit() %>% mutate(dummy=T) %>% 
   full_join(parameters %>% mutate(dummy=T),by="dummy") %>% select(-dummy) %>% 
   mutate(ng.injected = (conc)* #ng/uL
            (dil.factor^dil)* #unitless, thus still ng/uL
            (inj.vol) #volume injected, now ng
   ) %>% 
-  select(comp,dil,area,ng.injected) %>% 
-  filter(dil > 0)
+  select(batch,comp,dil,area,ng.injected) %>% 
+  mutate(log_inj = log(ng.injected),
+         log_area = log(area))
 
-calib.sum <- group_by(calib,comp) %>% do(fit=lm(ng.injected ~ area,.)) %>%
-  tidy(fit) %>% select(comp,term,estimate) %>% spread(term,estimate) %>% setNames(.,c("comp","intercept","slope")) %>%
-  gather(coef,value,intercept,slope) %>% group_by(coef) %>% mutate(mean = mean(value)) %>% spread(comp,value) %>%
-  gather(comp,value,-coef) %>% spread(coef,value)
+calib.sum <- group_by(calib,batch,comp) %>% do(fit=lm(log_inj ~ log_area,.)) %>%
+  tidy(fit) %>% select(batch,comp,term,estimate) %>% spread(term,estimate) %>% setNames(.,c("batch","comp","intercept","slope")) %>%
+  gather(coef,value,intercept,slope) %>% group_by(batch,coef) %>% mutate(mean = mean(value)) %>% spread(comp,value) %>%
+  gather(comp,value,-c(coef,batch)) %>% spread(coef,value)
 
-calib.check <- full_join(calib.sum %>% filter(comp!="mean"),calib,by="comp") %>%
-  mutate(residual = (slope*area+intercept) - ng.injected,
-         relative.residual = residual/ng.injected*100) %>% select(dil,comp,intercept,slope,residual,relative.residual) 
+calib.check <- full_join(calib.sum %>% filter(comp!="mean"),calib,by=c("batch","comp")) %>%
+  mutate(residual = exp((slope*log_area+intercept)) - ng.injected,
+         relative.residual = residual/ng.injected*100) %>% select(batch,dil,comp,intercept,slope,residual,relative.residual) 
 
 
 #Calibration Plot
-ggplot(calib,aes(area,ng.injected)) +
+ggplot(calib,aes(log_area,log_inj,color=batch)) +
   geom_point() +
-  stat_smooth(method = "lm", se = F,formula = y ~ x,color="blue") +
+  stat_smooth(method = "lm", se = F,formula = y ~ x) +
   facet_wrap(~comp,scales="free") +
   theme_bw() +
   theme(axis.title = element_text(size=18, vjust=0.25),
@@ -76,21 +77,19 @@ calib.check %>%
 #Clean-up from the calibration section
 rm(calib,calib.check)
 
+
 #######  STEP 2  #################   Data Analysis  ########################################
 
 data <- raw_data$gcfid %>% 
   spread(comp,area) %>% gather(comp,area,-c(id1,id2)) %>% mutate(area = ifelse(is.na(area),0,area)) %>% 
-  full_join(calib.sum,by="comp") %>%
+  full_join(raw_data$samples,by = c("id1", "id2")) %>%
+  full_join(calib.sum,by=c("batch","comp")) %>%
   filter(comp!="mean") %>% filter(!is.na(slope)) %>% 
   mutate(dummy=T) %>% full_join(parameters %>% mutate(dummy=T),by="dummy") %>% select(-dummy) %>% 
-  mutate(ng.injected = area * slope + intercept,
+  mutate(ng.injected = exp(log(area) * slope + intercept),
          ng.injected = ifelse(area == 0,0,ng.injected),
-         irs.theoretical = irs / irs.vol * irs.spike / residue.vol * inj.vol) %>% group_by(id1,id2) %>% 
-  mutate(ug.total = ng.injected / inj.vol * residue.vol / 1000) %>% # Placeholder ug.total calculation for samples without IRS
-  # mutate(recovery = ug.injected[which(comp == "EVAL")] / eval.theoretical,
-  #        mg.total = ug.injected / recovery / inj.vol * residue.vol) %>%
-  # select(sample,rep,comp,recovery,ug.total) %>% 
-  full_join(raw_data$samples,by = c("id1", "id2")) %>%
+         irs.theoretical = irs / irs.vol * irs.spike / volume * inj.vol) %>% group_by(id1,id2) %>% 
+  mutate(ug.total = ng.injected / inj.vol * volume / 1000) %>% 
   mutate(uggs = ug.total / mass_mg * 1000) %>%  #micrograms per gram sediment
   select(id1,id2,carbon_toc,comp,uggs) %>% 
   spread(comp,uggs) %>%
